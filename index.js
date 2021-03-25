d77d878577f8f4f04abe38ed1d5f7bfe76e5a45a
xMA3x <56519023+xMA3x@users.noreply.github.com>
2021-03-25 15:11:50 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const TeleBot = require('telebot');
-const bot = new TeleBot("1739845906:AAHm72IATwLhVGVU21huWX3YPqqBMEmuEzo");
+const bot = new TeleBot({
+    token: "1739845906:AAHm72IATwLhVGVU21huWX3YPqqBMEmuEzo",
+    polling: { // Optional. Use polling.
+    interval: 1000, // Optional. How often check updates (in ms).
+    timeout: 0, // Optional. Update polling timeout (0 - short polling).
+    // limit: 100, // Optional. Limits the number of updates to be retrieved.
+    retryTimeout: 5000, // Optional. Reconnecting timeout (in ms).
+    // proxy: 'http://username:password@yourproxy.com:8080' // Optional. An HTTP proxy to be used.
+    allowedUpdates: [],
+    },
+});
+
 
 bot.on('text', (msg) => msg.reply.text(msg.text));
 
